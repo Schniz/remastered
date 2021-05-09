717c17a25dbd43872836b85366afff9889d330ff
Gal Schlezinger <gal@spitfire.co.il>
2021-05-09 16:39:39 +0300

    this might work

--- api/serverless.js
+++ api/serverless.js
@@ -1,6 +1,6 @@
 import { renderRequest } from "../src/server";
 import manifest from "./ssr-manifest.json";
-import serverEntry from "../dist/server/entry-server";
+import * as serverEntry from "../dist/server/entry-server";
 import template from "../dist/server/template";
 
 export default async (req, res) => {
