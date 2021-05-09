0f26a48c4bd35bd66c38cd69b9fdc7b40c158db5
Gal Schlezinger <gal@spitfire.co.il>
2021-05-09 15:11:40 +0300

    okay

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,16 +1,9 @@
-// Require the framework
-import Fastify from "fastify";
 import { createServer } from "../src/server";
 
-// Instantiate Fastify with some config
-const app = Fastify({
-  logger: true,
-});
-
-// Register your application as a normal plugin.
-app.register(createServer);
+const server = createServer();
 
 export default async (req: unknown, res: any) => {
+  const app = await server;
   await app.ready();
   app.server.emit("request", req, res);
 };
