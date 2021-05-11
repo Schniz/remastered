59bebec6cfdc48bdd50dd0a182706eaa4e0cf3b8
Gal Schlezinger <gal@spitfire.co.il>
2021-05-11 18:57:37 +0300

    location

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 export default function Error404() {
-  return <h1>Oh no!!!!</h1>;
+  const { pathname } = useLocation();
+  return (
+    <>
+      <h1>Oh no!!!!</h1>
+      <p>
+        I don't know what the route <code>{pathname}</code> means.
+      </p>
+    </>
+  );
 }
