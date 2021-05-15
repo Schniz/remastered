0861c2085868c68bfb26f62673da9d177375ccde
Gal Schlezinger <gal@spitfire.co.il>
2021-05-15 20:06:52 +0300

    try to use 3rd party

--- packages/example-app/api/serverless.js
+++ packages/example-app/api/serverless.js
@@ -1,49 +1,12 @@
 // @ts-check
 
-import { renderRequest } from "@remastered/core/dist/src/server";
-import * as serverEntry from "../dist/server/entry.server";
+import createVercelFunction from "@remastered/vercel/dist/function";
 import { Request } from "node-fetch";
 import _ from "lodash";
-import fs from "fs-extra";
 import path from "path";
 
 /**
  * @param {Request} req
  * @param {import('@vercel/node').VercelResponse} res
  */
-export default async (req, res) => {
-  console.log({
-    cwd: process.cwd(),
-    dirname: __dirname,
-    keys: Object.keys(process.env),
-  });
-  const manifest = fs.readJsonSync(
-    path.join(process.cwd(), "../dist/client/ssr-manifest.json")
-  );
-  const clientManifest = fs.readJsonSync(
-    path.join(process.cwd(), "../dist/client/manifest.json")
-  );
-
-  const method = req.method.toUpperCase();
-  const request = new Request(req.url, {
-    method,
-    // @ts-ignore
-    body: method !== "GET" && method !== "HEAD" ? req : undefined,
-    headers: { ...req.headers },
-  });
-  const response = await renderRequest(
-    {
-      manifest,
-      serverEntry,
-      clientManifest,
-    },
-    // @ts-ignore
-    request
-  );
-
-  res.status(response.status);
-  for (const [header, value] of response.headers) {
-    res.setHeader(header, value);
-  }
-  res.end(response.body);
-};
+export default createVercelFunction(path.join(__dirname, ".."));
---
+++ packages/vercel/src/function.ts
@@ -0,0 +1,50 @@
+import { renderRequest } from "@remastered/core/dist/src/server";
+import { Request } from "node-fetch";
+import type { VercelApiHandler } from "@vercel/node";
+import fs from "fs-extra";
+import path from "path";
+import _ from "lodash";
+
+export default function createVercelFunction(
+  rootDir: string
+): VercelApiHandler {
+  const manifest$ = fs.readJson(
+    path.join(rootDir, "dist/client/ssr-manifest.json")
+  );
+  const clientManifest$ = fs.readJson(
+    path.join(rootDir, "dist/client/manifest.json")
+  );
+  const serverEntry$ = import(`${rootDir}/dist/server/server.entry.js`);
+
+  return async (req, res) => {
+    const [manifest, clientManifest, serverEntry] = await Promise.all([
+      manifest$,
+      clientManifest$,
+      serverEntry$,
+    ]);
+
+    const method = req.method?.toUpperCase() ?? "GET";
+    const request = new Request(req.url ?? "/", {
+      method,
+      // @ts-ignore
+      body: method !== "GET" && method !== "HEAD" ? req : undefined,
+      // @ts-ignore
+      headers: { ...req.headers },
+    });
+    const response = await renderRequest(
+      {
+        manifest,
+        serverEntry,
+        clientManifest,
+      },
+      // @ts-ignore
+      request
+    );
+
+    res.status(response.status);
+    for (const [header, value] of response.headers) {
+      res.setHeader(header, value);
+    }
+    res.end(response.body);
+  };
+}
