d5f4b76fdd32c37d327381259503233b487d29ee
Gal Schlezinger <gal@spitfire.co.il>
2021-05-15 20:55:41 +0300

    Add `@remastered/vercel` lib for Vercel deployment target (#2)

--- packages/example-app/api/serverless.js
+++ packages/example-app/api/serverless.js
@@ -1,39 +1,8 @@
 // @ts-check
 
-import { renderRequest } from "@remastered/core/dist/src/server";
-// @ts-ignore
-import manifest from "./ssr-manifest.json";
-// @ts-ignore
-import clientManifest from "./manifest.json";
-import * as serverEntry from "../dist/server/entry.server";
-import { Request } from "node-fetch";
-import _ from "lodash";
+import createVercelFunction from "@remastered/vercel/dist/function";
+import path from "path";
 
-/**
- * @param {Request} req
- * @param {import('@vercel/node').VercelResponse} res
- */
-export default async (req, res) => {
-  const method = req.method.toUpperCase();
-  const request = new Request(req.url, {
-    method,
-    // @ts-ignore
-    body: method !== "GET" && method !== "HEAD" ? req : undefined,
-    headers: { ...req.headers },
-  });
-  const response = await renderRequest(
-    {
-      manifest,
-      serverEntry,
-      clientManifest,
-    },
-    // @ts-ignore
-    request
-  );
+const rootDir = path.join(__dirname, "..");
 
-  res.status(response.status);
-  for (const [header, value] of response.headers) {
-    res.setHeader(header, value);
-  }
-  res.end(response.body);
-};
+export default createVercelFunction({ rootDir });
