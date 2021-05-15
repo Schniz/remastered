da2ab25d365b52bdb22ea96b71b3be9907f9e901
Gal Schlezinger <gal@spitfire.co.il>
2021-05-15 20:17:36 +0300

    broad

--- packages/example-app/api/serverless.js
+++ packages/example-app/api/serverless.js
@@ -1,12 +1,6 @@
 // @ts-check
 
 import createVercelFunction from "@remastered/vercel/dist/function";
-import { Request } from "node-fetch";
-import _ from "lodash";
 import path from "path";
 
-/**
- * @param {Request} req
- * @param {import('@vercel/node').VercelResponse} res
- */
 export default createVercelFunction(path.join(__dirname, ".."));
