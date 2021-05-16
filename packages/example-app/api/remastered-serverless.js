b3c46694e050dd8931ed778b44f25925b70705dd
Gal Schlezinger <gal@spitfire.co.il>
2021-05-16 13:11:46 +0300

    Apply vercel setup to example-app

--- packages/example-app/api/remastered-serverless.js
+++ packages/example-app/api/remastered-serverless.js
@@ -1,8 +1,4 @@
-// @ts-check
-
 import { createVercelFunction } from "@remastered/vercel";
 import path from "path";
-
 const rootDir = path.join(__dirname, "..");
-
 export default createVercelFunction({ rootDir });
