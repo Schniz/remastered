57b93364df95258a4043b512e6653b94e87c641c
Gal Schlezinger <gal@spitfire.co.il>
2021-05-18 10:14:08 +0300

    add a DefaultLayout

--- packages/remastered/src/App.tsx
+++ packages/remastered/src/App.tsx
@@ -3,10 +3,11 @@ import { useRoutes } from "react-router-dom";
 import { ErrorTracker } from "./ErrorTracker";
 import { Error404 } from "./Error404";
 import { routeElementsObject } from "./fsRoutes";
+import DefaultLayout from "./DefaultLayout";
 
-const UserLayout = Object.values(
-  import.meta.globEager("/app/layout.{t,j}sx")
-)[0].default;
+const UserLayout =
+  Object.values(import.meta.globEager("/app/layout.{t,j}sx"))[0].default ??
+  DefaultLayout;
 
 export default function App() {
   const element = useRoutes([
