3013c9535cc9e6757d44c24915f0585f563d6b03
Gal Schlezinger <gal@spitfire.co.il>
2021-05-20 17:56:33 +0300

    debugging serverless :facepalm:

--- packages/docs-site/api/list-files.js
+++ packages/docs-site/api/list-files.js
@@ -1,4 +1,5 @@
 import globby from "globby";
+import fs from "fs";
 
 /**
  * @param {import('@vercel/node').VercelRequest} req
@@ -7,10 +8,14 @@ import globby from "globby";
 export default async function listFiles(req, res) {
   const pattern = String(req.query.pattern ?? "/*");
   const files = await globby(pattern);
-  res.send({
-    pattern,
-    files,
-    cwd: process.cwd(),
-    dirname: __dirname,
-  });
+  if (files.length === 1) {
+    res.send(fs.readFileSync(files[0], "utf8"));
+  } else {
+    res.send({
+      pattern,
+      files,
+      cwd: process.cwd(),
+      dirname: __dirname,
+    });
+  }
 }
