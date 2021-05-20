ed828e734de22c88b5df58d5a71b77354711a912
Gal Schlezinger <gal@spitfire.co.il>
2021-05-20 14:34:43 +0300

    list files

---
+++ packages/docs-site/api/list-files.js
@@ -0,0 +1,16 @@
+import globby from "globby";
+
+/**
+ * @param {import('@vercel/node').VercelRequest} req
+ * @param {import('@vercel/node').VercelResponse} res
+ */
+export default async function listFiles(req, res) {
+  const pattern = String(req.query.pattern ?? "/*");
+  const [files] = await globby(pattern);
+  res.send({
+    pattern,
+    files,
+    cwd: process.cwd(),
+    dirname: __dirname,
+  });
+}
