036b6f88a8d851588657f60495f764c208fa77db
Gal Schlezinger <gal@spitfire.co.il>
2021-05-22 01:10:54 +0300

    Okay everything works, need to figure out why @tabler/icons is problematic and how we can inline onigasm or just add all the dependencies...

--- packages/docs-site/app/layout.tsx
+++ packages/docs-site/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Meta, Scripts, Links, Outlet } from "remastered";
-/* import "tailwindcss/tailwind.css"; */
+import "tailwindcss/tailwind.css";
 
 export default function Layout() {
   return (
