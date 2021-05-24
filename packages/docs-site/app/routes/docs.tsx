9fcaa131c5e855598e2534d7d7f7e52ab68f4331
Gal Schlezinger <gal@spitfire.co.il>
2021-05-24 12:15:17 +0300

    responsive docs menu (#14)

--- packages/docs-site/app/routes/docs.tsx
+++ packages/docs-site/app/routes/docs.tsx
@@ -12,6 +12,8 @@ import {
 } from "remastered";
 import _ from "lodash";
 import { docList, FileEntry } from "../docList";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import cx from "classnames";
 
 export const loader: LoaderFn<FileEntry[]> = async () => {
   return docList();
@@ -19,6 +21,18 @@ export const loader: LoaderFn<FileEntry[]> = async () => {
 
 export default function DocsLayout() {
   const files = useRouteData<FileEntry[]>();
+  const { MenuButton, isMenuOpen } = useMenuButton();
+  const directoryListingRef = React.useRef<HTMLDivElement>(null);
+
+  React.useLayoutEffect(() => {
+    if (!directoryListingRef.current) {
+      return;
+    }
+
+    if (isMenuOpen) {
+      directoryListingRef.current.focus();
+    }
+  }, [isMenuOpen]);
 
   return (
     <div className="flex flex-col w-screen h-screen">
@@ -39,11 +53,26 @@ export default function DocsLayout() {
           </a>
         </div>
       </div>
-      <div className="flex-1 mx-auto md:flex max-w-min">
-        <div className="p-2 pt-6 pr-8 md:w-72">
-          <DirectoryListing paths={files} />
+      <div className="flex-1 w-full mx-auto md:flex max-w-screen-lg">
+        <div className="w-screen md:w-auto">
+          <div
+            tabIndex={0}
+            ref={directoryListingRef}
+            className={cx(
+              "p-2 pt-4 pr-8 bg-white bg-opacity-80 z-10 backdrop-filter backdrop-blur-sm",
+              "fixed bottom-0 w-screen",
+              "md:static md:w-72 md:pt-6 md:block",
+              {
+                block: isMenuOpen,
+                hidden: !isMenuOpen,
+              }
+            )}
+          >
+            <DirectoryListing paths={files} />
+          </div>
+          <MenuButton />
         </div>
-        <div className="flex-1 w-full box-border">
+        <div className="flex-row flex-1 pb-10 overflow-scroll box-border md:pb-0">
           <Outlet />
         </div>
       </div>
@@ -58,6 +87,7 @@ function DirectoryListing({ paths }: { paths: FileEntry[] }) {
         if (path.type === "file") {
           return (
             <NavLink
+              tabIndex={0}
               key={path.link}
               to={path.link}
               className="block py-1 hover:underline hover:text-red-700 transition-all duration-75"
@@ -71,6 +101,7 @@ function DirectoryListing({ paths }: { paths: FileEntry[] }) {
             <React.Fragment key={path.title}>
               <div
                 key={path.title}
+                tabIndex={0}
                 className="pt-4 text-sm font-bold text-black text-opacity-75 first:pt-0"
               >
                 {path.title}
@@ -94,3 +125,33 @@ export const meta: MetaFn<unknown> = () => {
     description: `The documentation for Remastered, the real full-stack approach to React development.`,
   };
 };
+
+function useMenuButton(): {
+  MenuButton: React.ComponentType;
+  toggleMenu(): void;
+  isMenuOpen: boolean;
+} {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const toggleMenu = React.useCallback(() => {
+    setIsMenuOpen((v) => !v);
+  }, [setIsMenuOpen]);
+  const MenuButtonIcon = isMenuOpen ? XIcon : MenuIcon;
+
+  const output = React.useMemo(() => {
+    const MenuButton: React.FunctionComponent = () => {
+      return (
+        <button
+          aria-label="Toggle menu"
+          className="fixed bottom-0 right-0 z-50 block p-2 bg-white rounded-full bg-opacity-75 md:hidden"
+          onClick={toggleMenu}
+        >
+          <MenuButtonIcon className="w-8 h-8" />
+        </button>
+      );
+    };
+
+    return { MenuButton, toggleMenu, isMenuOpen };
+  }, [toggleMenu, MenuButtonIcon, isMenuOpen]);
+
+  return output;
+}
