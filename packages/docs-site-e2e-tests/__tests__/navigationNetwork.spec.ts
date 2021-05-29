7a367a6236963896fac13b5364e069c3508ba9b2
Gal Schlezinger <gal@spitfire.co.il>
2021-05-29 10:34:50 +0300

    use a stack

--- packages/docs-site-e2e-tests/__tests__/navigationNetwork.spec.ts
+++ packages/docs-site-e2e-tests/__tests__/navigationNetwork.spec.ts
@@ -4,36 +4,39 @@ import "expect-puppeteer";
 const browser = createBrowserBeforeAndAfter();
 
 test("going through history does not re-fetch data", async () => {
+  // initial page will not be "clicked"
+  // but will be used to determine what's in the history stack
+  const pagesToVisit = [`What is Remastered`, `Getting Started`, `Sessions`];
+
   const page = await browser.newPage();
   await page.goto("http://localhost:3000/docs");
+  await expect(page).toMatchElement("title", {
+    text: new RegExp(`${pagesToVisit[0]}`),
+  });
+
   const allRequests: string[] = [];
   page.on("request", (request) => {
     allRequests.push(request.url());
   });
 
-  await Promise.all([
-    page.waitForResponse((res) => res.url().endsWith(".json")),
-    expect(page).toClick("a", { text: `Getting Started` }),
-  ]);
-  await Promise.all([
-    page.waitForResponse((res) => res.url().endsWith(".json")),
-    expect(page).toClick("a", { text: `Session` }),
-  ]);
+  for (const pageToVisit of pagesToVisit.slice(1)) {
+    await Promise.all([
+      page.waitForResponse((res) => res.url().endsWith(".json")),
+      expect(page).toClick("a", { text: pageToVisit }),
+    ]);
+  }
 
   const historyRequests: string[] = [];
   page.on("request", (request) => {
     historyRequests.push(request.url());
   });
 
-  await page.goBack();
-  await expect(page).toMatchElement("title", {
-    text: /Getting Started/,
-  });
-
-  await page.goBack();
-  await expect(page).toMatchElement("title", {
-    text: /What is Remastered/,
-  });
+  for (const historyItem of [...pagesToVisit].reverse().slice(1)) {
+    await page.goBack();
+    await expect(page).toMatchElement("title", {
+      text: new RegExp(`${historyItem}`),
+    });
+  }
 
   expect(historyRequests).toEqual([]);
   expect(allRequests).toMatchInlineSnapshot(`
