6a04788dd28e32696d5bc828df135412ac356a04
Gal Schlezinger <gal@spitfire.co.il>
2021-06-03 16:00:07 +0300

    serve hmr on the same port as app

--- packages/remastered/src/server.tsx
+++ packages/remastered/src/server.tsx
@@ -7,6 +7,7 @@ import type { RenderFn } from "./entry-server";
 import _ from "lodash";
 import { getViteConfigPath } from "./getViteConfig";
 import type { HttpRequest, HttpResponse } from "./HttpTypes";
+import type { Server } from "http";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -31,9 +32,14 @@ function findDistRoot() {
   }
 }
 
-export async function createServer(root: string) {
+export async function createServer(
+  root: string,
+  port: number
+): Promise<{ app: Express.Application; server: Server }> {
   process.env.REMASTERED_PROJECT_DIR = root;
   const app = express();
+  let server = app.listen(port, "0.0.0.0");
+
   app.use(express.static(path.join(root, "public")));
 
   const vite = isProd
@@ -41,7 +47,10 @@ export async function createServer(root: string) {
     : await createViteServer({
         root,
         configFile: getViteConfigPath({ ssr: false }),
-        server: { middlewareMode: true },
+        server: {
+          middlewareMode: "ssr",
+          hmr: { server, port, path: "/@vite/_hmr_" },
+        },
       });
 
   const distRoot = findDistRoot();
@@ -74,7 +83,7 @@ export async function createServer(root: string) {
     res.writeHead(response.status, headers).end(response.body);
   });
 
-  return app;
+  return { app, server };
 }
 
 export async function renderRequest(
@@ -106,12 +115,12 @@ export async function renderRequest(
 }
 
 export async function main(root: string) {
-  const port = process.env.PORT || 3000;
-  console.log(`🌬 Warming up...`);
-  const app = await createServer(root);
-  console.log(`🎷 Pum, pum, pum! Server is playing at ${port}`);
-
-  app.listen(Number(port), "0.0.0.0");
+  const port = Number(process.env.PORT || 3000);
+  console.log(`🌬  Warming up...`);
+  await createServer(root, port);
+  console.log(
+    `🎷 Pum, pum, pum! Server is playing at http://localhost:${port}`
+  );
 }
 
 if (require.main === module) {
